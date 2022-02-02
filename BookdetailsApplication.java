9da185c2097a9ce0f394ac418050e379b6ced599
manjulanerati <98864517+manjulanerati@users.noreply.github.com>
2022-02-02 11:51:45 +0530

    book d

---
+++ BookdetailsApplication.java
@@ -0,0 +1,13 @@
+package com.book.java.bookdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdetailsApplication.class, args);
+	}
+
+}
